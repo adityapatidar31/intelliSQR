95403b322a6dcda13d00d565082ba52d68f76007
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-07 15:58:24 +0530

    create a route to create user

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 
+import userRouter from "./routes/userRoutes";
+
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
 
 app.get("/", (_req, res) => {
   res.status(200).json({
@@ -13,5 +21,7 @@ app.get("/", (_req, res) => {
     message: "Hii from the server",
   });
 });
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
