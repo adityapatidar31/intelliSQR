d48782c4cb568c172e41615564b410eced17c0c3
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-07 23:01:38 +0530

    crete the catchAsync to avoid try and catch

--- Backend/src/controllers/authController.ts
+++ Backend/src/controllers/authController.ts
@@ -3,54 +3,51 @@ import prisma from "../lib/prisma";
 import { verifyPassword } from "../utils/helper";
 import { hashPassword } from "../utils/helper";
 import { AppError } from "../utils/appError";
+import catchAsync from "../utils/catchAsync";
 
-export const loginUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    next(new AppError("Please Provide the Email and Password", 400));
-    return;
+export const loginUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      next(new AppError("Please Provide the Email and Password", 400));
+      return;
+    }
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user || !(await verifyPassword(password, user.password))) {
+      next(new AppError("Invalid Credentials", 400));
+      return;
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "You are logged in successfully",
+    });
   }
-  const user = await prisma.user.findUnique({
-    where: { email },
-  });
+);
 
-  if (!user || !(await verifyPassword(password, user.password))) {
-    next(new AppError("Invalid Credentials", 400));
-    return;
+export const signUpUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      next(new AppError("User already exist", 400));
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword },
+    });
+
+    res.status(201).json({ user });
   }
-
-  res.status(200).json({
-    status: "success",
-    message: "You are logged in successfully",
-  });
-};
-
-export const signUpUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log(req.body);
-  const { email, password } = req.body;
-
-  // Check if user already exists
-  const existingUser = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (existingUser) {
-    next(new AppError("User already exist", 400));
-    return;
-  }
-
-  const hashedPassword = await hashPassword(password);
-  const user = await prisma.user.create({
-    data: { email, password: hashedPassword },
-  });
-
-  res.status(201).json({ user });
-};
+);
